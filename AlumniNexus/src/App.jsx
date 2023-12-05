95ab0f9973e342966cbaf842a9151edfcb38f969
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-12-05 20:14:42 +0400

    Initial commit

---
+++ AlumniNexus/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-xl font-semibold text-red-800'>Hello, this is AlumniNexus</h1>
+    </>
+  )
+}
+
+export default App
