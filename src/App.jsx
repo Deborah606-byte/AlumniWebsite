7a7e6931df3a6890deff04de3b6c267ecd3a2bb0
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2023-12-06 14:15:45 +0000

    map routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import AboutUs from "./pages/about-us";
 import NotFound from "./pages/not-found";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
+import Home from "./pages/home";
+import Dashboard from "./pages/home";
+import Events from "./pages/home";
+import EventDetails from "./pages/home";
+import Opportunities from "./pages/home";
+import Stories from "./pages/home";
+import JobDetails from "./pages/home";
+import RSVP from "./pages/home";
 
 const App = () => {
   return (
@@ -17,6 +25,14 @@ const App = () => {
         </Route>
         <Route path={ROUTES.notFound} element={<NotFound />} />
       </Routes>
+      <Route path={ROUTES.home} element={<Home />} />
+      <Route path={ROUTES.dashboard} element={<Dashboard />} />
+      <Route path={ROUTES.events} element={<Events />} />
+      <Route path={ROUTES.eventDetails} element={<EventDetails />} />
+      <Route path={ROUTES.opportunity} element={<Opportunities />} />
+      <Route path={ROUTES.jobDetails} element={<JobDetails />} />
+      <Route path={ROUTES.stories} element={<Stories />} />
+      <Route path={ROUTES.rsvp} element={<RSVP />} />
     </Router>
   );
 };
