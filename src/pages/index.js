1fbcd49459cd33fbcc3ab7b29dd62b4dd746ffc8
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2023-12-10 10:32:18 +0000

    dashboard ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import AboutUsComponent from "../features/about-us/containers/about-us";
 import HomeComponent from "../features/user/home/containers/home";
 import EventComponent from "../features/event/containers/events";
 import EventDetailsComponent from "../features/event/containers/event-details";
+import DashboardComponent from "../features/user/dashboard/containers/dashboard";
 
 export { LoginComponent, SignUpComponent, AboutUsComponent, HomeComponent };
-export { EventComponent, EventDetailsComponent }
+export { EventComponent, EventDetailsComponent, DashboardComponent }
